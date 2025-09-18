0035b57e4b2b0aa64c8b50ba775d92695846c1a1
Vietduc <truongvietduc1992@gmail.com>
2025-09-18 16:43:49 +0700

    video30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,16 @@
+
+import './style.css';
 const MyComponent = () => {
     return (
-        <div> Truong Viet</div>
+        <>
+            <div> Truong Viet</div>
+            <div className='child'
+                style={
+                    { borderRadius: "3000px" }
+                }
+            >child</div>
+        </>
+
     );
 }
 export default MyComponent;
