b0dc0beaeaa59c73b0ea2db009a0f085ba287c27
Vietduc <truongvietduc1992@gmail.com>
2025-09-18 15:40:30 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+
+// () =>{}
+
+const App = () => {
   const [count, setCount] = useState(0)
+
+  // function myFunction() {
+  //   console.log(">>>run my arrow function")
+  // }
+  const myFunction = () => {
+    console.log(">>>run my arrow function")
+  }
+  myFunction()
+
 
   return (
     <>
@@ -16,7 +28,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Trương Việt Đức</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
